6369d05b8a5f9d2fa02115cce2e862fc800554a6
GitDaisy <100Daisy@protonmail.com>
2021-10-10 11:10:07 +0200

    clang: Drop proton-clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,8 +50,8 @@ TARGET_KERNEL_CONFIG := sleepy_defconfig
 TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CLANG_COMPILE := true
 KERNEL_LD := LD=ld.lld
-TARGET_KERNEL_CLANG_VERSION := proton
-KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
+#TARGET_KERNEL_CLANG_VERSION := proton
+#KERNEL_TOOLCHAIN_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin/aarch64-linux-gnu-
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
